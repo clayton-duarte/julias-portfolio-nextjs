4d306bd7da4de7065c4076be032259791d2327f5
cpd <cpd@duck.com>
2024-07-06 04:34:20 +0000

    feat: about page v1

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,30 @@
+import Image from 'next/image'
+
+import about from '~/assets/about.png'
+import Grid from '~/components/Grid'
 import Header from '~/components/Header'
+import Text from '~/components/Text'
 
 export default function AboutPage() {
   return (
     <>
       <Header />
-      <h1>About Page</h1>
+      <Grid container p="3rem">
+        <Grid xs='minmax(auto, 550px) 1fr' gap="3rem" align='center'>
+          <Image src={about} alt='Julia Santos' priority />
+          <Grid gap="2rem">
+            <Text.H3>
+              UX Designer, Research enthusiast & Graphic Designer
+            </Text.H3>
+            <Text.H5 bold={false}>
+              Hello, I’m Julia, a UX Designer and research enthusiast with a solid foundation in graphic design, digital marketing, and sales.
+            </Text.H5>
+            <Text.H5 bold={false}>
+              Originally from Brazil, I’ve been living in Canada since 2019. In my spare time, you can find me playing board games with friends, riding my bicycle, enjoying museums, or just shooting the breeze about this, that, and the other thing.
+            </Text.H5>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   )
 }
